c24274ef3453e8f6f2382b7ccb2b21177a35cc1c
parthzagmag <37842842+parthzagmag@users.noreply.github.com>
2018-03-30 13:32:01 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,69 @@
 import React, { Component } from 'react';
 import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
 
-export default class SimpleButton extends Component {
-	render() {
-		return( 
-		    <View>
-				<Button onPress={() => { Alert.alert('You tapped the button!'); }} title="Press Me" /> 
-			</View>	
-		);
-	}
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
+   onPressButton2() {
+    Alert.alert('You presses button in row no _')
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        
+		<View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me big blue"
+          />
+        </View>
+		
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me big marun"
+            color="#841584"
+          />
+        </View>
+		
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={this.onPressButton2}
+            title="This looks great!"
+          />
+          <Button
+            onPress={this.onPressButton2}
+            title="OK!"
+            color="#841584"
+          />
+        </View>
+		
+      </View>
+    );
+  }
 }
 
+const styles = StyleSheet.create({
+  
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  
+  buttonContainer: {
+    margin: 20
+  },
+  
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+  
+})
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => ButtonBasics);
